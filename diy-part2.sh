0a1a1d01f5c1e4a276143ab5e0ecc158d76c8982
woni928 <398401414@qq.com>
2024-02-27 14:44:12 +0800

    2024-0227

--- diy-part2.sh
+++ diy-part2.sh
@@ -28,7 +28,3 @@ sed -i 's/192.168.1.1/192.168.123.2/g' package/base-files/files/bin/config_gener
 #sed -i 's/stripped/release/g' feeds/packages/multimedia/aliyundrive-webdav/Makefile
 sed -i 's#SYNC#SYNC -D_LARGEFILE64_SOURCE#g' feeds/packages/utils/xfsprogs/Makefile
 sed -i 's/REENTRANT -D_GNU_SOURCE/LARGEFILE64_SOURCE/g' feeds/packages/lang/perl/perlmod.mk
-
-# 需要 golang 1.20.x 或最新版本（需要 openwrt-21.02 或更旧版本）
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
