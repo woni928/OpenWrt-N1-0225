cd3cf7b817166748521c0902f5ce17ebd541dfae
woni928 <398401414@qq.com>
2024-03-11 11:21:13 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,3 +13,4 @@ rm -rf package-temp
 git clone https://github.com/ophub/luci-app-amlogic.git  package-temp/luci-app-amlogic
 mv -f package-temp/luci-app-amlogic/luci-app-amlogic package/lean/
 rm -rf package-temp
+
