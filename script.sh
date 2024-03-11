30a2e96241ad9c5ea01c36d4f21c3b7d7bbf6fa3
woni928 <398401414@qq.com>
2024-03-11 15:07:37 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,10 +13,3 @@ rm -rf package-temp
 git clone https://github.com/ophub/luci-app-amlogic.git  package-temp/luci-app-amlogic
 mv -f package-temp/luci-app-amlogic/luci-app-amlogic package/lean/
 rm -rf package-temp
-
-# 临时修复acpid,aliyundrive-webdav,xfsprogs,perl-html-parser 导致的编译失败问题
-#sed -i 's#flto#flto -D_LARGEFILE64_SOURCE#g' feeds/packages/utils/acpid/Makefile
-#sed -i 's/stripped/release/g' feeds/packages/multimedia/aliyundrive-webdav/Makefile
-#sed -i 's#SYNC#SYNC -D_LARGEFILE64_SOURCE#g' feeds/packages/utils/xfsprogs/Makefile
-sed -i 's/REENTRANT -D_GNU_SOURCE/LARGEFILE64_SOURCE/g' feeds/packages/lang/perl/perlmod.mk
-
